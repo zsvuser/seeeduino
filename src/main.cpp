a66bda2cabcac23e90b0dbe87da92e4b033fc7a2
zsvuser <sezverev@mail.ru>
2022-02-23 15:47:42 -0500

    master

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
-#include <Arduino.h>
-#include <Wire.h>
 #include "Seeed_BME280.h"
+#include <Wire.h>
 
 BME280 bme280;
 
-void setup() {
-  // put your setup code here, to run once:
-
+void setup()
+{
   Serial.begin(9600);
   if(!bme280.init()){
     Serial.println("Device error!");
   }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
   float pressure;
-
+  
   //get and print temperatures
   Serial.print("Temp: ");
   Serial.print(bme280.getTemperature());
   Serial.println("C");//The unit for  Celsius because original arduino don't support speical symbols
-
+  
   //get and print atmospheric pressure data
   Serial.print("Pressure: ");
   Serial.print(pressure = bme280.getPressure());
